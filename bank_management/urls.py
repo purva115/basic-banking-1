9a4d0c20714de1a40c1677a07e1d921ab0d6e21e
Purva Jagtap <purvajagtap115@gmail.com>
2021-03-11 23:53:05 +0530

    added

--- bank_management/urls.py
+++ bank_management/urls.py
@@ -21,7 +21,7 @@ from django.views.static import serve
 
 
 urlpatterns = [
-    path('/', include('banking.urls')),
+    path('', include('banking.urls')),
 
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root':       settings.MEDIA_ROOT}),
