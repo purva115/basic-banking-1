25a54eef5d1fc0c43dfb5f800131cf04cf2924cf
Purva Jagtap <purvajagtap115@gmail.com>
2021-03-11 23:26:41 +0530

    added

--- bank_management/settings.py
+++ bank_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,17 +78,8 @@ WSGI_APPLICATION = 'bank_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bank',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'purva123',
-        'auth_plugin': 'mysql_native_password',
-        'OPTIONS': {
-            'read_default_file': '/path/to/my.cnf',
-            'init_command': 'SET default_storage_engine=INNODB'
-
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -129,5 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
